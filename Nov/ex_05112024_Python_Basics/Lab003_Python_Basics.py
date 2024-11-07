6ba982f348e2be2996774127f7cbfd95e3beecb5
Promode <pramoddutta_96@yahoo.com>
2024-11-07 08:21:45 +0530

    7th nov

--- Nov/ex_05112024_Python_Basics/Lab003_Python_Basics.py
+++ Nov/ex_05112024_Python_Basics/Lab003_Python_Basics.py
@@ -1,4 +1,10 @@
-print(2-2)
-print(2+2)
-print(2*2)
-print(2/2) # 1.0?
+# print("Hello World")
+# print("Pramod Dutta")
+# print(self, *args, sep=' ', end='\n', file=None):
+# print(, *args, sep=' ', end='\n', file=None):
+# *args - unlimited number of comma seperated arguments
+#     print("Pramod Dutta",123,"Amit","John") # IndentationError: unexpected indent
+
+print("Pramod", 123, "Amit", "John")
+print("Pramod", 123, "Amit", "John", sep='*',end="      ")
+print("Pramod", 123, "Amit", "John", sep='*')
