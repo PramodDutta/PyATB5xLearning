9fc2483a1e7b4ff2581e7a4352c87311865e2853
Promode <pramoddutta_96@yahoo.com>
2024-11-12 08:07:41 +0530

    12th Nov

--- Nov/ex_09112024_Literals_Variables/Lab030_Task2.py
+++ Nov/ex_09112024_Literals_Variables/Lab030_Task2.py
@@ -8,3 +8,14 @@
 
 
 
+num1 = int(input("Enter the first number (dividend): "))
+num2 = int(input("Enter the second number (divisor): "))
+
+quotient = num1 // num2  # Using floor division to get the quotient
+remainder = num1 % num2   # Using modulo to get the remainder
+
+#divmod()
+
+
+print(f"Quotient (Q) -> {quotient}")
+print(f"Remainder (R) -> {remainder}")
